44ccbaf64d3b64dd79bf9390798999f46aaee67b
Natasha-eng <natasha.shilova.89@gmail.com>
2020-12-17 15:19:41 +0300

    creating structure of the project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import Header from "./header/Header";
+import Main from "./main/Main";
+import Skills from "./skills/Skills";
 
 function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header/>
+            <Main/>
+            <Skills/>
+        </div>
+    );
 }
 
 export default App;
